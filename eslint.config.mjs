2397ad3824fcff339ff13747586f499d99b80605
Peter Savchenko <specc.dev@gmail.com>
2024-04-20 16:11:15 +0300

    add nodejs config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import TypeScript from './src/configs/typescript.mjs';
 import JsDoc from './src/configs/jsdoc.mjs';
 import CodeStyle from './src/configs/codestyle.mjs';
 import Vue from './src/configs/vue.mjs';
+import Node from './src/configs/node.mjs';
 import Globals from './src/configs/globals.mjs';
 import Ignore from './src/configs/ignore.mjs';
 
@@ -12,6 +13,7 @@ export default [
   ...CodeStyle,
   ...JavaScript,
   ...TypeScript,
+  ...Node,
   ...JsDoc,
   ...Vue,
 ];
